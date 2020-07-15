b6d88762d586d139b5c2267eddc7b71b392f09ce
Diffa <ddiffa2@gmail.com>
2020-07-15 21:45:43 +0700

    add search bar

--- lib/api/http_helper.dart
+++ lib/api/http_helper.dart
@@ -5,9 +5,10 @@ import 'package:movie_db/model/movie.dart';
 
 class HttpHelper {
   final String urlKey = 'api_key=ac313fc1138a0ed697567a0dedddc6cd';
-  final String urlBase = 'https://api.themoviedb.org/3/movie';
-  final String urlUpcoming = '/upcoming?';
+  final String urlBase = 'https://api.themoviedb.org/3/';
+  final String urlUpcoming = 'movie/upcoming?';
   final String urlLanguage = '&language=en-US';
+  final String urlSearchBase = 'search/movie?';
 
   Future<List> getUpcoming() async {
     final String upcoming = urlBase + urlUpcoming + urlKey + urlLanguage;
@@ -15,6 +16,19 @@ class HttpHelper {
     //waits for a Future to complete. It won't go to next line of its thread until it completes this line
     http.Response result = await http.get(upcoming);
 
+    if (result.statusCode == HttpStatus.ok) {
+      final jsonResponse = json.decode(result.body);
+      final moviesMap = jsonResponse['results'];
+      List movies = moviesMap.map((i) => Movie.fromJson(i)).toList();
+      return movies;
+    } else {
+      return null;
+    }
+  }
+
+  Future<List> findMovies(String title) async {
+    final String query = urlBase + urlSearchBase + urlKey + "&query=" + title;
+    http.Response result = await http.get(query);
     if (result.statusCode == HttpStatus.ok) {
       final jsonResponse = json.decode(result.body);
       final moviesMap = jsonResponse['results'];
--- lib/screen/movie_list.dart
+++ lib/screen/movie_list.dart
@@ -13,8 +13,12 @@ class _MovieListState extends State<MovieList> {
   List _movies;
   HttpHelper _helper;
 
+  Icon visibleIcon = Icon(Icons.search);
+  Widget searchBar = Text('Movies');
+
   final String iconBase = 'https://image.tmdb.org/t/p/w92/';
-  final String defaultImage = 'https://images.freeimages.com/images/large-previews/5eb/movie-clapboard-1184339.jpg';
+  final String defaultImage =
+      'https://images.freeimages.com/images/large-previews/5eb/movie-clapboard-1184339.jpg';
 
   @override
   void initState() {
@@ -38,25 +42,46 @@ class _MovieListState extends State<MovieList> {
 
     return Scaffold(
       appBar: AppBar(
-        title: Text('Movies'),
+        title: searchBar,
+        actions: <Widget>[
+          IconButton(
+            icon: this.visibleIcon,
+            onPressed: () {
+              setState(() {
+                if (this.visibleIcon.icon == Icons.search) {
+                  this.visibleIcon = Icon(Icons.cancel);
+                  this.searchBar = TextField(
+                    textInputAction: TextInputAction.search,
+                    onSubmitted: (String text) {
+                      search(text);
+                    },
+                    style: TextStyle(color: Colors.white, fontSize: 20.0),
+                  );
+                } else {
+                  setState(() {
+                    this.visibleIcon = Icon(Icons.search);
+                    this.searchBar = Text('Movies');
+                  });
+                }
+              });
+            },
+          )
+        ],
       ),
       body: ListView.builder(
         itemBuilder: (BuildContext context, int position) {
-          if(_movies[position].posterPath != null){
-            image = NetworkImage(
-              iconBase + _movies[position].posterPath
-            );
-          }else{
+          if (_movies[position].posterPath != null) {
+            image = NetworkImage(iconBase + _movies[position].posterPath);
+          } else {
             image = NetworkImage(defaultImage);
           }
           return Card(
             color: Colors.white,
             elevation: 2.0,
             child: ListTile(
-              onTap: (){
+              onTap: () {
                 MaterialPageRoute route = MaterialPageRoute(
-                  builder: (_) => MovieDetailScreen(_movies[position])
-                );
+                    builder: (_) => MovieDetailScreen(_movies[position]));
                 Navigator.push(context, route);
               },
               leading: CircleAvatar(
@@ -71,5 +96,13 @@ class _MovieListState extends State<MovieList> {
         itemCount: (this._moviesCount == null) ? 0 : this._moviesCount,
       ),
     );
+  }
+
+  Future search(text) async {
+    _movies = await _helper.findMovies(text);
+    setState(() {
+      _moviesCount = _movies.length;
+      _movies = _movies;
+    });
   }
 }
